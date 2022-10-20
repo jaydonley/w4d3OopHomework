072820a2afa91718a5248457baa6cfe3beb37efb
Jason Donley <jay.donley@gmail.com>
2022-10-20 15:26:54 -0400

    w4d3OopHomework

---
+++ app.js
@@ -0,0 +1,61 @@
+
+/* ---------------#3------------------------ */
+
+let characters = [
+  ["Mario", "Plumber"], 
+  ["Luigi", "Brother"], 
+  ["Boo", "Ghost"]
+];
+
+/*----------------#4------------------------- */
+
+let characters2 = [
+  ["Mario", "Plumber"], 
+  ["Luigi", "Brother"], 
+  ["Boo", "Ghost"]
+];
+function spaceLoopy(arr){
+
+
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log(arr[i][j]);
+    }
+  }
+}
+
+spaceLoopy(characters2);
+
+/* -------------------#5--------------------- */
+
+  function greaterNum(arr1, arr2) {
+
+          if (arr1 > arr2) {
+       console.log("This number is greater.") 
+      }
+      else {
+        console.log( "this second number is greater.")
+      }
+    }
+    greaterNum(55, 88);
+  
+  /* -----------------#6-------------------- */
+
+ 
+  function sameString(arr11, arr22){
+    
+      if (arr11 == arr22) {
+        console.log ("You know these things are equal.");
+      }
+      else {
+      console.log ("No way, man. Way off from square.");
+      }
+     }
+     sameString("Homie", "Holmes");
+
+      /* RYAN, THANK YOU SO VERY MUCH FOR THE HELP IN GETTING THIS THROUGH MY THINK DOME. MUCH APPRECIATED!!! -JD */
+    
+      
+  
+  
